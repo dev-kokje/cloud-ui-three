1f02d1d53333dcc9fa909bddb6002e95d5fba90e
dev-kokje <dev-kokje@gmail.com>
2023-04-20 10:42:14 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import { StrictMode } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StrictMode>
+      <h1>Hello Cloud World</h1>
+      <Button variant='primary'>click me</Button>
+    </StrictMode>
   );
 }
 
